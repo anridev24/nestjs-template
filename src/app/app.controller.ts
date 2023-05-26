d84d4407775a38098f8e947875ac220daeea8130
Andronik Nazaryan <anrinazaryan@gmail.com>
2023-05-26 22:51:13 +0400

    refactor: heath route path change

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -5,7 +5,7 @@ import { Controller, Get } from '@nestjs/common';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('health')
   health(): string {
     return this.appService.health();
   }
