99c8488ee43a8e5b62c1724fd45b46f1fe274257
Andronik Nazaryan <anrinazaryan@gmail.com>
2023-05-26 22:28:46 +0400

    feat: add exception filter

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
+import { AllExceptionsFilter, useRequestLogging } from './common';
 import { AppConfig } from '@app/app.config';
 import { AppModule } from '@app/app.module';
 import { ConfigService } from '@nestjs/config';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { Logger, ValidationPipe } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
 import { NestFastifyApplication } from '@nestjs/platform-fastify';
 import { clusterize } from './utils';
-import { useRequestLogging } from './common';
 import helmet from 'helmet';
 
 const { CLUSTERING, PORT } = process.env;
@@ -17,6 +17,7 @@ const bootstrap = async () => {
   );
 
   const configService = app.get(ConfigService);
+  const httpAdapter = app.get(HttpAdapterHost);
 
   const BASE_PATH = configService.get('BASE_PATH');
   const NODE_ENV = configService.get('NODE_ENV');
@@ -24,13 +25,13 @@ const bootstrap = async () => {
 
   app.enableVersioning();
   app.enableCors();
-
   app.use(helmet());
   useRequestLogging(app, LOG_LEVEL);
 
   app.setGlobalPrefix(BASE_PATH);
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
 
   await app.listen(PORT, () => {
     Logger.log(`Listening at Port ${PORT}`, bootstrap.name);
